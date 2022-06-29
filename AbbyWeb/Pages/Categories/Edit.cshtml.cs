0cda71108652072dac96c8d2b81aa1d17a5a02c3
DanniB2023 <65875267+DanniB2023@users.noreply.github.com>
2022-06-29 09:20:39 -0400

    change

--- AbbyWeb/Pages/Categories/Edit.cshtml.cs
+++ AbbyWeb/Pages/Categories/Edit.cshtml.cs
@@ -1,3 +1,4 @@
+
 using AbbyWeb.Data;
 using AbbyWeb.Model;
 using Microsoft.AspNetCore.Mvc;
@@ -7,31 +8,31 @@ namespace AbbyWeb.Pages.Categories;
 
 [BindProperties]
 
-public class CreateModel : PageModel
+public class EditModel : PageModel
 {
     private readonly ApplicationDbContext _db;
     public Category Category { get; set; }
 
-    public CreateModel(ApplicationDbContext db)
+    public EditModel(ApplicationDbContext db)
     {
         _db = db;
     }
-    public void OnGet()
+    public void OnGet(int id)
     {
+        Category = _db.Category.Find(id);
     }
 
     public async Task<IActionResult> OnPost()
     {
         if (Category.Name == Category.DisplayOrder.ToString())
         {
-            ModelState.AddModelError(Category.Name, "The DisplayOrder cannot exactly match the Name.");
+            ModelState.AddModelError(String.Empty, "The DisplayOrder cannot exactly match the Name. ");
         }
 
         if (ModelState.IsValid)
         {
-            await _db.Category.AddAsync(Category);
+            _db.Category.Update(Category);
             await _db.SaveChangesAsync();
-            TempData["success"] = "Category created successfully";
             return RedirectToPage("Index");
 
         }
--- AbbyWeb/Pages/Categories/Index.cshtml.cs
+++ AbbyWeb/Pages/Categories/Index.cshtml.cs
@@ -17,6 +17,7 @@ namespace AbbyWeb.Pages.Categories
         public void OnGet()
         {
             Categories = _db.Category;
+
         }
     }
 }
